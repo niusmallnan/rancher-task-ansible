fdaea454b993c55428e1fc8cfcbd435be0bdb521
niusmallnan <zhangzhibo521@gmail.com>
2017-01-29 20:08:21 +0800

    init

---
+++ ansible_plugins/lookup_plugins/rancher_hosts.py
@@ -0,0 +1,36 @@
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+
+HAVE_RANCHER_CLIENT=False
+try:
+    import gdapi
+    HAVE_RANCHER_CLIENT=True
+except ImportError:
+    pass
+
+from ansible.errors import AnsibleError
+from ansible.plugins.lookup import LookupBase
+
+
+class LookupModule(LookupBase):
+
+    def run(self, terms, variables=None, **kwargs):
+
+        if not HAVE_RANCHER_CLIENT:
+            raise AnsibleError("Can't LOOKUP(rancher_envs): module gdapi is not installed")
+
+        ret = []
+        for term in terms:
+            rancher_url = term["url"]
+            access_key = term["access_key"]
+            secret_key = term["secret_key"]
+
+            client = gdapi.Client(url=rancher_url,
+                                  access_key=access_key,
+                                  secret_key=secret_key)
+
+            for env in client.list_host():
+                ret.append(env.id)
+
+        return ret
