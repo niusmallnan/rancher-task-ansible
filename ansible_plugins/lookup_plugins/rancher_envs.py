c3bd151131c56659069041e468f5b33f04680858
niusmallnan <zhangzhibo521@gmail.com>
2017-01-30 09:59:33 +0800

    just use dict

--- ansible_plugins/lookup_plugins/rancher_envs.py
+++ ansible_plugins/lookup_plugins/rancher_envs.py
@@ -31,8 +31,8 @@ class LookupModule(LookupBase):
                                   secret_key=secret_key)
 
             for env in client.list_project():
-                ret.append({"id", env.id,
-                            "name", env.name,
-                            "state", env.state})
+                ret.append({"id": env.id,
+                            "name": env.name,
+                            "state": env.state})
 
         return ret
